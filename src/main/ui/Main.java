6aa3d2111ddce7355c2584e54d572d1313367b21
melouie <melouie@students.cs.ubc.ca>
2019-09-14 15:39:58 -0700

    WIP 2 - fixed checkstyles

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -12,10 +12,14 @@ public class Main {
 
     public static void initiateApplication() {
         System.out.println("What would you like to do?");
+        printInstructions();
+    }
+
+    public static void printInstructions() {
         System.out.println("Press [1] to add a new borehole log.");
         System.out.println("Press [2] to view list of existing borehole logs.");
         System.out.println("Press [3] to delete an existing borehole log.");
-//        System.out.println("Press [4] to edit an existing log.");
+        System.out.println("Type 'quit' to end the application.");
         printOptions();
     }
 
@@ -24,7 +28,6 @@ public class Main {
         String userinput = input.nextLine();
         ArrayList<String> logs = new ArrayList();
 
-
         if (userinput.equals("1")) {
             System.out.println("Please enter new borehole ID.");
             Scanner bhid = new Scanner(System.in);
@@ -32,10 +35,13 @@ public class Main {
             System.out.println(logs + "end of list");
         } else if (userinput.equals("2")) {
             System.out.println(logs);
+        } else if (userinput.equals("quit")) {
+            System.out.println("Quitting");
         } else {
             System.out.println("Sorry I didn't understand the command, please try again.");
         }
     }
 }
+
 
 
