308c040c8a32d382cda5f8ce2c9f91b57feba988
melouie <melouie@students.cs.ubc.ca>
2019-09-15 16:33:44 -0700

    Added option [3] - functionality not complete.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,6 @@
 package ui;
 
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,13 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to BH Log Generator v.1.0!");
-//        ArrayList<String> logs = new ArrayList<String>();
-        Main sm = new Main();
-        sm.initiateApplication();
+        Main main = new Main();
+        main.initiateApplication();
     }
 
     public void initiateApplication() {
-        Scanner input = new Scanner(System.in);
 
         System.out.println("What would you like to do?");
         System.out.println("Press [1] to add a new borehole log.");
@@ -24,16 +21,8 @@ public class Main {
         System.out.println("Press [3] to delete an existing borehole log.");
         System.out.println("Type 'quit' to end the application.");
         handleUserInput();
-//        printInstructions();
     }
 
-//    public static void printInstructions() {
-//        System.out.println("Press [1] to add a new borehole log.");
-//        System.out.println("Press [2] to view list of existing borehole logs.");
-//        System.out.println("Press [3] to delete an existing borehole log.");
-//        System.out.println("Type 'quit' to end the application.");
-//        printOptions();
-//    }
 
     public void handleUserInput() {
         String str = "";
@@ -46,10 +35,13 @@ public class Main {
             } else if (str.equals("2")) {
                 optionTwo();
             } else if (str.equals("3")) {
-                System.out.println("Select log to delete.");
+                optionThree();
+            } else if (!str.equals("quit")) {
+                invalidInput();
             }
 
             if (str.equals("quit")) {
+                System.out.println("Goodbye.");
                 break;
             }
         }
@@ -70,4 +62,23 @@ public class Main {
         initiateApplication();
     }
 
+    public void optionThree() {
+        System.out.println("Please enter the name of the log you would like to delete.");
+        Scanner deletebh = new Scanner(System.in);
+//        if (logs.contains(deletebh.nextLine())) {
+        logs.remove(deletebh);
+        System.out.println("You successfully removed " + deletebh.nextLine());
+        System.out.println(logs);
+        initiateApplication();
+//    } else {
+////        System.out.println("Sorry, that log does not exist.");
+////        initiateApplication();
+    }
+
+
+    public void invalidInput() {
+        System.out.println("Sorry, I didn't understand. Please try a different command.");
+        initiateApplication();
+    }
 }
+
